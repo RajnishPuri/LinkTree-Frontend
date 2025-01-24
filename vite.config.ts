bee83ae186d1a656fd8e31cd957db5b34f1f8784
Rajnish Puri <rajnishpuri15@gmail.com>
2025-01-25 03:25:09 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173, // Default to 5173 if PORT is not set
+    host: '0.0.0.0', // Listen on all network interfaces
+  },
   build: {
-    outDir: 'dist', // default is 'dist', but it's good to confirm
+    outDir: 'dist',
   },
 });
